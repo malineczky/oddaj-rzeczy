707fd00d717055c1cc226e7ce8e0a1675950533d
malineczky <aleksandra.m.kot@gmail.com>
2023-08-07 13:03:45 +0200

    section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.scss";
 import HomeHeader from "./components/HomeHeader";
+import HomeThreeColumn from "./components/HomeThreeColumns";
 
 function App() {
-  return <HomeHeader />;
+  return (
+    <>
+      <HomeHeader />
+      <HomeThreeColumn />
+    </>
+  );
 }
 export default App;
---
+++ src/components/HomeThreeColumns.jsx
@@ -0,0 +1,35 @@
+function HomeThreeColumn() {
+  return (
+    <>
+      <section className="counter">
+        <div className="counter__container">
+          <div className="counter__box">
+            <h1>10</h1>
+            <p className="counter__box--title">ODDANYCH WORKÓW</p>
+            <p className="counter__box--desc">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+          <div className="counter__box">
+            <h1>5</h1>
+            <p className="counter__box--title">WSPARTYCH ORGANIZACJI</p>
+            <p className="counter__box--desc">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+          <div className="counter__box">
+            <h1>7</h1>
+            <p className="counter__box--title">ZORGANIZOWANYCH ZBIÓREK</p>
+            <p className="counter__box--desc">
+              Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+              enim a elit viverra elementuma. Aliquam erat volutpat.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+export default HomeThreeColumn;
